50f6519d9ffb7683163d78e7b28a5a70d0860bfd
JorgeCruz19 <jcruz@DESKTOP-L3R68CG.localdomain>
2020-10-31 19:01:39 -0600

    Init commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const menu = document.querySelector('.header')
+
+window.addEventListener('scroll', function () {
+	let desplazamientoY = pageYOffset
+	if (desplazamientoY > 100) {
+		menu.classList.add('opaque')
+	} else {
+		menu.classList.remove('opaque')
+	}
+})
+
+$('a').click(function () {
+	var sectionTo = $(this).attr('href')
+	$('.navbar-collapse').collapse('hide')
+	$('html, body').animate(
+		{
+			scrollTop: $(sectionTo).offset().top,
+		},
+		1000
+	)
+})
